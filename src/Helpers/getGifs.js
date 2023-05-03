4a3f3ae249c7ada728a4c23cf9ee1f557bee8988
paolaguerra <paolaguerraph@gmail.com>
2023-05-03 11:45:56 -0400

    Pruebas en el helper getGifs

--- src/Helpers/getGifs.js
+++ src/Helpers/getGifs.js
@@ -1,5 +1,5 @@
 
-export const GetGifs = async({category}) => {
+export const getGifs = async({category}) => {
 
     const url = `https://api.giphy.com/v1/gifs/search?q=${ encodeURI(category) }&limit=4&api_key=CcdfUnivGYoURQcawOVTwed7q57fVgvM`;
     const resp = await fetch(url); // Await va a delante de mi codigo asincronico // Fetch resuelve la promesa
