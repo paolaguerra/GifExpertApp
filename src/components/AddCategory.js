cbfbc2190cd23968f3f3e27edb3e8b314a6a5164
paolaguerra <paolaguerraph@gmail.com>
2023-06-28 13:18:15 -0400

    style updates

--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -18,12 +18,23 @@ export const AddCategory = (props) => {
 
   return (
     <form onSubmit={handleSubmit} aria-label="form">
-      <input
+      {/* <input
         type="text"
         placeholder="search gif"
         value={inputValue}
         onChange={handleImputChange}
-      />
+      /> */}
+      <div className="input-group flex-nowrap">
+        <input
+          type="text"
+          className="form-control"
+          placeholder="search gif"
+          value={inputValue}
+          onChange={handleImputChange}
+          aria-label="Username"
+          aria-describedby="addon-wrapping"
+        />
+      </div>
     </form>
   );
 };
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 export const GifGridItem = ({title, url, id}) => {
 
   return (
+    
     <div className="card">
-        <img src={url} alt={title} />
-        <p>{title}</p>
+        <img className="card-img-top" src={url} alt={title} />
+        <div class="card-body">
+              <p class="card-text">{title}</p>
+            </div>
     </div>
   )
 }
+
+
+        {/* <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="..." />
+            <div class="card-body">
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+        </div> */}
 
 GifGridItem.prototype = {
   title: PropTypes.string.isRequired,
